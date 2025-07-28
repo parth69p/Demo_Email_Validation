1e00e5e6b89031b352b94ecc6b672dba1e136132
parth69p <82306394+parth69p@users.noreply.github.com>
2025-07-28 15:58:56 +0530

    Add files via upload
    This is the testing class of MsTest for email validation

---
+++ Test1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demo_Email_Validation;
+
+namespace EmailValidation.MsTestProject
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private EmailValidator Validator;
+        [TestInitialize]
+       public void TestInitialize()
+        {
+            // Initialization code can go here if needed
+            Validator = new EmailValidator();
+        }
+
+        [TestMethod]
+        public void IsEmail_shouldReturnFalseForInvalidEmail()
+        {
+            string invalidEmail = "invalid-email.com";
+            bool result = Validator.IsValidEmail(invalidEmail);
+           Assert.IsFalse(result);// we are checkin for valid email value
+        }
+
+        [TestMethod]
+        public void IsEmail_shouldReturnTrueForValidEmail()
+        {
+            string validEmail = "Ivalid@example.com";
+            bool result = Validator.IsValidEmail(validEmail);
+            Assert.IsTrue(result); // we are checkin for valid email value
+        }
+    }
+}
