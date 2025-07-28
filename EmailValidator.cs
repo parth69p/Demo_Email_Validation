492092339caeb2aa1c08dafa7644a465662b2221
parth <parth@DESKTOP-31NEM17>
2025-07-28 15:52:45 +0530

    Add project files.

---
+++ EmailValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace Demo_Email_Validation
+{
+    public class EmailValidator
+    {
+        public bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+            // ^ - start of the string
+            //[^@\s]+ - one or more characters that are not @ or whitespace(\s)
+            // $ - end of the string
+            // @ - at symbol
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            // Mobile no: @"^[6-9]\d{9}$"
+            return Regex.IsMatch(email, pattern);// defined insided the Regex Package
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// steps for implementing Email validation logic using Mstest and xUnit
+// case study: as a developer:
+// I Want to validate if an email string is valid or not
+// step 1: create a EmailValidator class (using regex)
+//using System.Text.RegularExpressions;
+// step 2: create a MS Test implementation project
+// step 3: create a xUnit implementation project based on followwing package
+// xunit.runner.visualstudio,  xunit, microsoft.net.test.sdk
